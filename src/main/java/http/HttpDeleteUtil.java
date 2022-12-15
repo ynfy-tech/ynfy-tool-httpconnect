6b4371f45c2a1eeb635d6fc4f1fe721273f93b58
hsiong <peak1203@126.com>
2022-12-15 15:15:03 +0800

    optimize code comment to match the maven release code style need

--- src/main/java/http/HttpDeleteUtil.java
+++ src/main/java/http/HttpDeleteUtil.java
@@ -19,6 +19,9 @@ import java.util.Map;
  */
 public class HttpDeleteUtil extends InitUtil {
 
+    /**
+     * 内部静态类
+     */
     private static class Inner {
         private static final HttpDeleteUtil instance = new HttpDeleteUtil();
     }
@@ -27,7 +30,7 @@ public class HttpDeleteUtil extends InitUtil {
      * 单例模式之：静态内部类单例模式
      * 只有第一次调用getInstance方法时，虚拟机才加载 Inner 并初始化instance ，只有一个线程可以获得对象的初始化锁，其他线程无法进行初始化，
      * 保证对象的唯一性。目前此方式是所有单例模式中最推荐的模式，但具体还是根据项目选择。
-     * @return
+     * @return 返回单例
      */
     public static HttpDeleteUtil getInstance() {
         return Inner.instance;
